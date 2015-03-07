4d00fbed10763fa4fa6fe1acfd426b5220f0143c
Kirill Gerasimenko <kirill.gerasimenko@outlook.com>
2015-03-07 23:15:49 +0200

    WIP on new options

--- src/RunAsClient/Program.cs
+++ src/RunAsClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Mono.Options;
 using RunAsClient.Core;
 using RunAsClient.Properties;
 
@@ -6,41 +7,84 @@ namespace RunAsClient
 {
     class Program
     {
-        public static int Main(string[] args)
+        private static void ShowUsage(OptionSet options)
         {
-            var options = new Options();
-            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            Console.WriteLine("usage: RunAsClient [OPTIONS]");
+            Console.WriteLine("Runs provided command with specified credentials");
+            Console.WriteLine();
+            Console.WriteLine("OPTIONS:");
+            options.WriteOptionDescriptions(Console.Out);
+        }
+
+        public static void Main(string[] args)
+        {
+            var showUsage = false;
+            var domain = string.Empty;
+            var username = string.Empty;
+            var password = string.Empty;
+            var command = string.Empty;
+
+            var options = new OptionSet()
             {
-                if (string.IsNullOrWhiteSpace(options.Domain))
-                {
-                    Console.WriteLine(Resources.InvalidDomainOption);
-                    return -1;
-                }
+                { "help|h|?", "show usage and exit", h => showUsage = h != null },
+                { "d|domain=", "domain name", d => domain = d },
+                { "u|username=", "user name in domain", u => username = u },
+                { "p|password=", "user's domain password", p => password = p },
+                { "c|command=", "command to be executed under user's credentials", c => command = c }
+            };
 
-                if (string.IsNullOrWhiteSpace(options.UserName))
-                {
-                    Console.WriteLine(Resources.InvalidUsernameOption);
-                    return -1;
-                }
-
-                if (string.IsNullOrWhiteSpace(options.Password))
-                {
-                    Console.WriteLine(Resources.InvalidPasswordOption);
-                    return -1;
-                }
-
-                if (string.IsNullOrWhiteSpace(options.Command))
-                {
-                    Console.WriteLine(Resources.InvalidCommandOption);
-                    return -1;
-                }
-
-                return RunAsLauncher.LaunchCommand(options.Command, 
-                                                   options.Domain, 
-                                                   options.UserName, 
-                                                   options.Password);
+            try
+            {
+                options.Parse(args);
             }
-            return -1;
+            catch (OptionException e)
+            {
+                Console.Write(@"RunAsClient: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(
+                    "Try 'RunAsClient --help' for more information");
+
+                return;
+            }
+
+            if (showUsage)
+            {
+                ShowUsage(options);
+                return;
+            }
+
+
+/*
+            if (string.IsNullOrWhiteSpace(options.Domain))
+            {
+                Console.WriteLine(Resources.InvalidDomainOption);
+                return -1;
+            }
+
+            if (string.IsNullOrWhiteSpace(options.UserName))
+            {
+                Console.WriteLine(Resources.InvalidUsernameOption);
+                return -1;
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Password))
+            {
+                Console.WriteLine(Resources.InvalidPasswordOption);
+                return -1;
+            }
+
+            if (string.IsNullOrWhiteSpace(options.Command))
+            {
+                Console.WriteLine(Resources.InvalidCommandOption);
+                return -1;
+            }
+
+            return RunAsLauncher.LaunchCommand(options.Command, 
+                                               options.Domain, 
+                                               options.UserName, 
+                                               options.Password);
+            }
+*/
         }
     }
 
